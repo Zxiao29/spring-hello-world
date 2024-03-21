8d1080c3945bff5701d58f8242601b91d35fb1c7
fsannicolo <federico.sannicolo@marconirovereto.it>
2024-03-21 13:45:49 +0100

    Basic get request and with parameters

--- src/main/java/it/marconi/helloworld/controllers/HelloController.java
+++ src/main/java/it/marconi/helloworld/controllers/HelloController.java
@@ -2,6 +2,7 @@ package it.marconi.helloworld.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 // stereotype annotation
 @Controller
@@ -9,4 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     
     // CRUD (Create Read Update Delete) -> HTTP (POST, GET, PUT, DELETE)
+
+    @GetMapping
+    public String greet() {
+        return "hello-world";
+    }
+
+    @GetMapping(params = "papero")
+    public String greetParameter() {
+        return "hello-world-param";
+    }
+
+    // MVC
+    
 }
